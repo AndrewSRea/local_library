f0caf519be214206a5f0a4d17f586e9e6e07ea3d
AndrewSRea <andrew.s.rea@gmail.com>
2022-03-09 13:57:30 -0800

    Added an '.env' file and updated configuration settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$^9wvs$wzv3yufiz8)=mwdq@gc-v%xk*pamyunk@5dcm4ap1ep'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['peaceful-ridge-89504.herokuapp.com']
 
 
 # Application definition
